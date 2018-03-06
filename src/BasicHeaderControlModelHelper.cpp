97a3c2dbe84ab2996067715dad64a985b90965e8
Dmitry Lisachenko <xdemonx07@gmail.com>
2018-03-07 01:34:43 +0300

    BasicHeaderControlModelHelper moved under QtTools namespace

--- src/BasicHeaderControlModelHelper.cpp
+++ src/BasicHeaderControlModelHelper.cpp
@@ -1,18 +1,20 @@
 #include <QtTools/BasicHeaderControlModelHelper.hqt>
 
-const QString BasicHeaderControlModelHelper::CodeListMime::MimeCodeFormat =
-	QStringLiteral("application/x-BasicHeaderControlModel-ColumnCodes");
-
-const QStringList BasicHeaderControlModelHelper::CodeListMime::MimeFormats =
-	QStringList(BasicHeaderControlModelHelper::CodeListMime::MimeCodeFormat);
-
-bool BasicHeaderControlModelHelper::CodeListMime::hasFormat(const QString & mimetype) const
+namespace QtTools
 {
-	return mimetype == MimeCodeFormat;
-}
+	const QString BasicHeaderControlModelHelper::CodeListMime::MimeCodeFormat =
+		QStringLiteral("application/x-BasicHeaderControlModel-ColumnCodes");
 
-QStringList BasicHeaderControlModelHelper::CodeListMime::formats() const
-{
-	return MimeFormats;
-}
+	const QStringList BasicHeaderControlModelHelper::CodeListMime::MimeFormats =
+		QStringList(BasicHeaderControlModelHelper::CodeListMime::MimeCodeFormat);
 
+	bool BasicHeaderControlModelHelper::CodeListMime::hasFormat(const QString & mimetype) const
+	{
+		return mimetype == MimeCodeFormat;
+	}
+
+	QStringList BasicHeaderControlModelHelper::CodeListMime::formats() const
+	{
+		return MimeFormats;
+	}
+}
